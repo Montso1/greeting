ab15dfeb0d72cb0d178e17356b16d027a63ea8bd
David <davidbahula8@gmail.com>
2026-07-30 16:03:04 +0200

    Add files via upload

---
+++ gretting.py
@@ -0,0 +1,10 @@
+#Assignment - basic
+
+name = "David"
+print(f"Hello, my name is {name}")
+
+#Assignment - slightly advanced
+
+course = "Python Programming"
+year = 2026
+print(f"Hello World! I am studying {course} in year {year}")
